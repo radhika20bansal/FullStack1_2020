9782bab1aa9007743e045bed44160dc3226d93bf
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-11-08 18:16:27 +0530

    Practical 16 committed

--- Practicals/JS_Files/todo.js
+++ Practicals/JS_Files/todo.js
@@ -1,56 +1,52 @@
-//Selectors
-
+// Selectors
 var todoInput = document.querySelector(".todo-input");
-var todoButton = document.querySelector(".todo-button");
-var todoList = document.querySelector(".todo-list");
+var btn = document.querySelector("button");
+var todolist = document.querySelector(".todo-list");
 
 //Event Handlers
-todoButton.onclick = create;
-todoList.onclick = checkdelete;
-
+btn.onclick = create;
+todolist.onclick = deletecheck;
 
 //Functions
 function create(e) {
     if (todoInput.value != "") {
         e.preventDefault();
 
-        var newDiv = document.createElement(div);
+        var newDiv = document.createElement("div");
         newDiv.classList.add("todo");
 
-
-        var newLi = document.createElement(li);
-        newLi.classList.add("todo-items");
+        var newLi = document.createElement("li");
+        newLi.classList.add("todo-item");
         newLi.innerHTML = todoInput.value;
         newDiv.appendChild(newLi);
 
-
-        var checkbtn = document.createElement(button);
+        var checkbtn = document.createElement("button");
         checkbtn.classList.add("check-btn");
         checkbtn.innerHTML = '<i class="fa fa-check"></i>';
         newDiv.appendChild(checkbtn);
 
-        var deletebtn = document.createElement(button);
+        var deletebtn = document.createElement("button");
         deletebtn.classList.add("delete-btn");
         deletebtn.innerHTML = '<i class="fa fa-trash"></i>';
         newDiv.appendChild(deletebtn);
 
-        todoList.appendChild(newDiv);
+        todolist.appendChild(newDiv);
         todoInput.value = "";
     } else {
-        alert("Input Field can't be blank. Please enter a appropriate value.")
+        alert("Input Field Can't Be Blank");
     }
 }
 
-function checkdelete(e) {
+function deletecheck(e) {
     var item = e.target;
 
     if (item.classList[0] === "delete-btn") {
         var parent = item.parentElement;
         parent.remove();
     }
+
     if (item.classList[0] === "check-btn") {
         var parent = item.parentElement;
-        parent.classList.toggle("final");
+        parent.classList.toggle("completed");
     }
-
 }
